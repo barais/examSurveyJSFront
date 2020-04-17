51c310bfe09c8286c4602d565316cdcab7f292e9
Olivier Barais <olivier.barais@gmail.com>
2020-04-17 09:53:11 +0200

    initial commit
    SurveyJS with Angular with three new widget
    uml
    takephoto
    and markdown with highlightjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'examSIR';
+export class AppComponent implements OnInit {
+
+constructor(private http: HttpClient){
+
+
+}
+
+casusername ;
+
+ngOnInit(): void {
+  this.http.get('/casusername', {responseType: 'text'}).subscribe(data => {
+    console.log(data);
+    this.casusername = data.split('\n').shift();
+  });
+
+}
+
+
+
+  sendData(result) {
+    console.log(result);
+    const formData = new FormData();
+    let dataStr = new Blob([JSON.stringify(result)], { type: 'application/json' });
+    //var dataStr = "data:application/json;charset=utf-8," + encodeURIComponent(JSON.stringify(result));
+
+    formData.append('upload[]', dataStr, 'result.json');
+
+
+    this.http.post('/upload', formData).subscribe(
+      (res) => console.log(res),
+      (err) => console.log(err)
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SurveyComponent } from './survey.component';
+import { SurveyCreatorComponent } from './survey.creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SurveyComponent,
+    SurveyCreatorComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
